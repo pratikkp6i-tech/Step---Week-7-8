5977c4a48fbc5a794f2a0a2fc575a3eedc7c2b86
pratikkp6i-tech <pratikkp6i@gmail.com>
2026-04-28 16:32:35 +0530

    NaturalSum

---
+++ Level1/NaturalSum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NaturalSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        
+        System.out.println("The sum of " + n + " natural numbers is: " + findSum(n));
+        sc.close();
+    }
+
+    public static int findSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
